c7b5f0784575249de1b8385b56f0bece2bf70235
Industria-Digital-Cetim <90331645+Industria-Digital-Cetim@users.noreply.github.com>
2021-09-22 08:31:16 +0200

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,14 @@
+var boton = document.getElementById('btBuscar');
+
+boton.addEventListener('click', async () => {
+	
+	var ejemplo = document.getElementById('ejemplo').value;
+	var ejemplo2 = document.getElementById('ejemplo2').value;
+	console.log("ejemplo 1**********");
+	console.log(ejemplo);
+	console.log(typeof(ejemplo));
+	console.log("ejemplo 2**********");
+	console.log(ejemplo2);
+	console.log(typeof(ejemplo2));
+	alert("HOLA");
+});
